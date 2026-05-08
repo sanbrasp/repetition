88fc5e6960033007ba471b4659ee7fa7f27f1811
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-05-08 20:32:59 +0200

    feat: create Magazine.cs, inherit from baseclass, implement interface

---
+++ Models/Magazine.cs
@@ -0,0 +1,32 @@
+﻿using OperationalBackendProgrammingRepetition.Interfaces;
+
+namespace OperationalBackendProgrammingRepetition.Models;
+
+public class Magazine : LibraryItem, ISearchable
+{
+    private int _issueNumber;
+    private string _publisher;
+    private string _title;
+    
+    public int IssueNumber => _issueNumber;
+    public string Publisher => _publisher;
+    public string Title => _title;
+    
+    public Magazine(string title, int issueNumber, string publisher, int year) : 
+        base(title, year)
+    {
+        if (string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException(nameof(title), "Title cannot be empty.");
+        if (string.IsNullOrWhiteSpace(publisher)) throw new ArgumentNullException(nameof(publisher), "Publisher name cannot be empty.");
+        
+        _issueNumber = issueNumber;
+        _publisher = publisher;
+    }
+
+    public override string GetItemType() => "Magazine";
+
+    public bool MatchesQuery(string query)
+    {
+        string q = query.Trim().ToLower();
+        return Title.ToLower().Contains(q) || Publisher.ToLower().Contains(q);
+    }
+}
