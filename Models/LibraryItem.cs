c36905ee3b37226c2b7569e28d6a769850d7ee31
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-05-08 20:33:30 +0200

    feat: create LibraryItem.cs baseclass

---
+++ Models/LibraryItem.cs
@@ -0,0 +1,45 @@
+﻿namespace OperationalBackendProgrammingRepetition.Models;
+
+public abstract class LibraryItem
+{
+    protected Guid _id { get; init; }
+
+    public bool IsAvailable;
+
+    internal string Title { get; init; }
+    
+    protected int Year { get; init; }
+
+
+    protected LibraryItem(string title, int year)
+    {
+        _id = new Guid();
+        Title = title;
+        Year = year;
+        IsAvailable = true;
+    }
+    
+    
+    public bool SetAvailable()
+    {
+        if (IsAvailable == false)
+            IsAvailable = true;
+        return IsAvailable;
+    }
+
+    public bool SetUnavailable()
+    {
+        if (IsAvailable)
+            IsAvailable = false;
+        return IsAvailable;
+    }
+
+    public abstract string GetItemType();
+
+    public override string ToString()
+    {
+        return ($"Title: {Title}\n" +
+                $"Year: {Year}\n" + 
+                $"IsAvailable: {IsAvailable}");
+    }
+}
