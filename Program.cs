774e02c5ed742209a5630ff6f5a46398a189b1a5
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-05-10 14:18:29 +0200

    Instantiate objects in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿using OperationalBackendProgrammingRepetition.Catalogs;
+using OperationalBackendProgrammingRepetition.Services;
 using OperationalBackendProgrammingRepetition.UI;
 
-ConsoleMenu menu = new ConsoleMenu();
+var catalog = new Catalog();
+var loanService = new LoanService();
+var memberService = new MemberService();
+
+ConsoleMenu menu = new ConsoleMenu(catalog, loanService, memberService);
 
 
 menu.RunMenu();
