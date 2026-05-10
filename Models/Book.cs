ad01f22e3aa93cff52dcdcc8988aad275a80b791
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-05-10 14:18:09 +0200

    fix: isbn from int to string. add guard clauses.

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,20 +4,21 @@ namespace OperationalBackendProgrammingRepetition.Models
 {
     public class Book : LibraryItem, ISearchable
     {
-        private int _isbn;
+        private string _isbn;
         private string _author;
         private string _title;
 
-        public int ISBN => _isbn;
+        public string ISBN => _isbn;
         public string Author => _author;
         public string Title => _title;
 
 
-        public Book(string title, string author, int year, int isbn)  : 
+        public Book(string title, string author, int year, string isbn)  : 
             base(title, year)
         {
             if (string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException(nameof(title), "Title cannot be empty.");
-            if (isbn < 0) throw new ArgumentOutOfRangeException(nameof(isbn), "ISBN cannot be less than 0.");
+            if (string.IsNullOrWhiteSpace(author)) throw new ArgumentNullException(nameof(author), "Author cannot be empty.");
+            if (string.IsNullOrWhiteSpace(isbn)) throw new ArgumentNullException(nameof(isbn), "ISBN cannot be empty.");
             
             _title = title;
             _isbn = isbn;
