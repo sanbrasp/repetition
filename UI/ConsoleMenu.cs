f88f0a356bd79d8a4415431685bb5a2f7452a6ad
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-05-10 14:15:08 +0200

    fix: clean up menu, add new methods, wire up

--- UI/ConsoleMenu.cs
+++ UI/ConsoleMenu.cs
@@ -1,44 +1,76 @@
 ﻿using System.Globalization;
 using Arbeidskrav1_Sem2.Helpers;
 using OperationalBackendProgrammingRepetition.Catalogs;
+using OperationalBackendProgrammingRepetition.Models;
+using OperationalBackendProgrammingRepetition.Services;
 
 namespace OperationalBackendProgrammingRepetition.UI;
 
 public class ConsoleMenu
 {
-    Catalog _catalog = new Catalog();
+    private readonly Catalog _catalog;
+    private readonly LoanService _loanService;
+    private readonly MemberService _memberService;
+
+    public ConsoleMenu(Catalog catalog, LoanService loanService, MemberService memberService)
+    {
+        _catalog = catalog;
+        _loanService = loanService;
+        _memberService = memberService;
+    }
     
     public void RunMenu()
     {
-        
-        
-        bool showMenu = true;
-        while (showMenu)
+        bool running = true;
+        while (running)
         {
-            Console.Clear();
-            Console.WriteLine("== Library Menu ==");
-            Console.WriteLine();
+            ShowMenu();
+            
 
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1. Add Book");
-            Console.WriteLine("2. List all books");
-            Console.WriteLine("3. Search by author");
-            Console.WriteLine("0. Quit");
-
-            var choice = InputHelpers.ReadMenuChoice("Choice: ", 0, 3);
+            var choice = InputHelpers.ReadMenuChoice("Choice: ", 0, 7);
 
             switch (choice)
             {
                 case 1:
                     AddBook(); break;
                 case 2:
-                    ListAllBooks(); break;
+                    ListAllItems(); break;
                 case 3:
-                    SearchByAuthor(); break;
+                    SearchItems(); break;
+                case 4: 
+                    RegisterMember(); break;
+                case 5:
+                    LoanItem();  break;
+                case 6:
+                    ReturnItem(); break;
+                case 7:
+                    GetMemberLoans(); break;
                 case 0:
-                    showMenu = false; break;
+                    running = false; break;
+                default:
+                    Console.WriteLine("Invalid choice"); 
+                    break;
             }
         }
+
+        Console.WriteLine("Goodbye!");
+    }
+
+    private void ShowMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("== Library Menu ==");
+        Console.WriteLine();
+
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Add book");
+        Console.WriteLine("2. List all items");
+        Console.WriteLine("3. Search.");
+        Console.WriteLine("4. Register member");
+        Console.WriteLine("5. Loan item");
+        Console.WriteLine("6. Return item");
+        Console.WriteLine("7. View member loan");
+        Console.WriteLine("0. Quit");
     }
 
     private void AddBook()
@@ -48,7 +80,7 @@ public class ConsoleMenu
         var title = InputHelpers.ReadRequiredString("Enter book title: ");
         var author = InputHelpers.ReadRequiredString("Enter book author: ");
         var year = InputHelpers.ReadInt("Enter book year: ");
-        var isbn = InputHelpers.ReadInt("Enter book ISBN: ");
+        var isbn = InputHelpers.ReadRequiredString("Enter book ISBN: ");
         Console.WriteLine();
         _catalog.AddBook(title, author, year, isbn);
         
@@ -57,7 +89,7 @@ public class ConsoleMenu
         Console.ReadKey();
     }
 
-    private void ListAllBooks()
+    private void ListAllItems()
     {
         Console.Clear();
         Console.WriteLine("== All Books ==");
@@ -71,18 +103,101 @@ public class ConsoleMenu
         Console.ReadKey();
     }
 
-    private void SearchByAuthor()
+    private void RegisterMember()
     {
         Console.Clear();
-        Console.WriteLine("== Books by author ==");
-        var author = InputHelpers.ReadRequiredString("Enter book author: ");
-        foreach (var book in _catalog.FindBookByAuthor (author))
+        string name = InputHelpers.ReadRequiredString("Enter member name: ");
+        string email = InputHelpers.ReadRequiredString("Enter member email: ");
+
+        try
         {
-            Console.WriteLine(author);
-            Console.WriteLine();
+            var member = _memberService.RegisterMember(name, email);
+            Console.WriteLine($"Registered: {member.GetDisplayName()} (ID: {member.MemberId}.");
         }
-        
-        Console.WriteLine("Press any key to return to menu.");
-        Console.ReadKey();
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+
+    private void LoanItem()
+    {
+        string isbn = InputHelpers.ReadRequiredString("Enter ISBN: ");
+        string memberId =  InputHelpers.ReadRequiredString("Enter member id: ");
+
+        try
+        {
+            var item = _catalog.FindByISBN(isbn)
+                ?? throw new InvalidOperationException($"Item with ISBN {isbn} not found.");
+            var member = _memberService.FindById(memberId)
+                ?? throw new InvalidOperationException($"Item with ID {memberId} not found.");
+            
+            _loanService.LoanItem(item, member);
+            Console.WriteLine($"Loaned '{item.Title} to {member.GetDisplayName()}'");
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+
+    private void ReturnItem() // Who would return a book based on the ISBN...?
+    {
+        string isbn = InputHelpers.ReadRequiredString("Enter ISBN: ");
+
+        try
+        {
+            var item = _catalog.FindByISBN(isbn)
+                ?? throw new InvalidOperationException($"Item with ISBN {isbn} not found.");
+            
+            _loanService.ReturnItem(item);
+            Console.WriteLine($"'{item.Title}' returned.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+
+    private void SearchItems()
+    {
+        Console.Clear();
+        string query =  InputHelpers.ReadRequiredString("Search: ");
+        var results = _catalog.Search(query);
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+
+    private void GetMemberLoans()
+    {
+        string memberId = InputHelpers.ReadRequiredString("Enter member id: ");
+
+        try
+        {
+            var member = _memberService.FindById(memberId)
+                         ?? throw new InvalidOperationException($"Member with ID {memberId} not found.");
+
+            var loans = _loanService.GetLoansForMember(member);
+
+            if (loans.Count == 0)
+            {
+                Console.WriteLine($"{member.GetDisplayName()} has no active loans.");
+                return;  // semicolon, not colon
+            }
+
+            Console.WriteLine($"Active loans for {member.GetDisplayName()}:");
+            foreach (var loan in loans)
+            {
+                string overdue = loan.IsOverdue ? " OVERDUE" : string.Empty;
+                Console.WriteLine($"  - '{loan.Item.Title}' due {loan.DueDate:dd MMM yyyy}{overdue}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
     }
 }
